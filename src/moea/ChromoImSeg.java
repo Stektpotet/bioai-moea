fb1aad21094d72ccf72e576f785bddd820922e18
Klara Schlueter <klaramsc@stud.ntnu.no>
2021-04-10 16:40:31 +0200

    Implement conversion from genotype to phenotype

--- src/moea/ChromoImSeg.java
+++ src/moea/ChromoImSeg.java
@@ -1,20 +1,20 @@
 package moea;
 
-import moea.ProblemImSeg;
 import ga.data.Chromosome;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class ChromoImSeg implements Chromosome<ProblemImSeg> {
 
-    private EdgeOut[] genes;
+    private EdgeOut[] genotype;
     private List<Set<Integer>> segments;
+    private boolean changed;
+    private List<Set<Integer>> phenotype;
 
     public ChromoImSeg(ProblemImSeg image) {
-        this.genes = new EdgeOut[image.getPixelCount()];
+        this.genotype = new EdgeOut[image.getPixelCount()];
+        this.changed = true;
     }
 
     @Override
@@ -22,11 +22,79 @@ public class ChromoImSeg implements Chromosome<ProblemImSeg> {
         return 0;
     }
 
+    // TODO: does this ensure the immutability of the return value?
+    final List<Set<Integer>> phenotype(ProblemImSeg image) throws Exception {
+
+        if (!changed) {
+            return phenotype;
+        }
+
+        Set<Integer> unvisited = IntStream.range(0, genotype.length).collect(HashSet::new, Set::add, Set::addAll);
+        List<Set<Integer>> segments = new ArrayList<>();
+
+        while (!unvisited.isEmpty()) {
+            Integer element = unvisited.stream().findAny().get();
+            unvisited.remove(element);
+
+            Set<Integer> currentSegment = new HashSet<>();
+            currentSegment.add(element);
+
+            Integer predecessor;
+            while (true) {
+                predecessor = element;
+                element = pointsTo(image, predecessor);
+
+                if (!currentSegment.add(element)) {
+                    segments.add(currentSegment);
+                    break;
+                } else if (unvisited.remove(element)) {
+                    boolean terriblyWrong = true;
+                    for (Set<Integer> segment : segments) {
+                        if (segment.contains(element)) {
+                            segment.addAll(currentSegment);
+                            terriblyWrong = false;
+                            break;
+                        }
+                    }
+                    if (terriblyWrong) {
+                        throw new Exception("Element was in visited, but not in any segment!");
+                    }
+                    break;
+                } else {
+                    currentSegment.add(element);
+                }
+            }
+        }
+        phenotype = segments;
+        return Collections.unmodifiableList(segments);
+    }
+
+    private Integer pointsTo(ProblemImSeg image, Integer pixelIdx) {
+        EdgeOut direction = genotype[pixelIdx];
+        int width = image.getWith();
+
+        int pointsTo;
+        switch (direction) {
+            case UP:
+                pointsTo = pixelIdx - width;
+                return pointsTo < 0 ? pixelIdx : pointsTo;
+            case DOWN:
+                pointsTo = pixelIdx + width;
+                return pointsTo > genotype.length ? pixelIdx : pointsTo;
+            case RIGHT:
+                return pixelIdx % width == 0 ? pixelIdx : pixelIdx + 1;
+            case LEFT:
+                pointsTo = pixelIdx - 1;
+                return pointsTo % width == 0 ? pixelIdx : pointsTo;
+            default:
+                return pixelIdx;
+        }
+    }
+
     private enum EdgeOut {
         RIGHT,
         LEFT,
         UP,
         DOWN,
-        NONE
     }
 }
