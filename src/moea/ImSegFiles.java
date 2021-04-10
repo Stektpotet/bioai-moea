ca1b304aa474a3ff41e371c58eb4c1782361c41e
Halvor Smedås <stektpotet@gmail.com>
2021-04-10 14:23:39 +0200

    Adds ImSegProblem and a way to read them in from images

--- src/moea/ImSegFiles.java
+++ src/moea/ImSegFiles.java
@@ -10,21 +10,9 @@ import java.io.IOException;
 
 public class ImSegFiles {
 
-    public static Image ReadImSegProblem(String filepath) throws IOException {
+    public static ImSegProblem ReadImSegProblem(String filepath) throws IOException {
         BufferedImage image = ImageIO.read(new File(filepath));
         int[] argb = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
-        Image img = new Image(intARGBtoByteRGB(argb));
-
-    }
-
-    private static byte[] intARGBtoByteRGB(final int[] argb) {
-        // https://www.javaer101.com/en/article/46458013.html
-        Pixel[] rgb = new Pixel[argb.length];
-
-        for (int i = 0; i < argb.length; i++) {
-            rgb[4 * i] = (byte) ((argb[i] >> 16) & 0xff); // R
-            rgb[4 * i + 1] = (byte) ((argb[i] >> 8) & 0xff); // G
-            rgb[4 * i + 2] = (byte) ((argb[i]) & 0xff); // B
-        }
+        return new ImSegProblem(new Image(argb, image.getWidth(), image.getHeight()));
     }
 }
--- src/moea/ImSegProblem.java
+++ src/moea/ImSegProblem.java
@@ -1,4 +1,16 @@
 package moea;
 
+import collections.Image;
+import collections.Pixel;
+
 public class ImSegProblem {
+    private final Image image;
+
+    public ImSegProblem(Image image) {
+        this.image = image;
+    }
+
+    public final Pixel getPixel(int flatIndex) {
+        return image.getPixel(flatIndex);
+    }
 }
