ded403dcec8cf0630a96b4617e00b81d7a8f7a2a
Klara Schlueter <klaramsc@stud.ntnu.no>
2021-04-25 10:51:08 +0200

    Implement survivor selection as described in MOEA paper

---
+++ src/ga/nsga2/SurvivorSelectorMOEA.java
@@ -0,0 +1,46 @@
+package ga.nsga2;
+
+import collections.ParetoImSeg;
+import ga.selection.SurvivorSelector;
+import moea.ChromoImSeg;
+import moea.ProblemImSeg;
+import moea.UtilChromoImSeg;
+import moea.ga.PopulationImSeg;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SurvivorSelectorMOEA implements SurvivorSelector<ProblemImSeg, PopulationImSeg, ChromoImSeg> {
+
+    @Override
+    public PopulationImSeg select(PopulationImSeg generation, List<ChromoImSeg> parents, List<ChromoImSeg> offspring) {
+        int size = generation.size();
+        ProblemImSeg problem = generation.getProblem();
+
+        List<ChromoImSeg> poolIndividuals = new ArrayList<>(generation.size() + offspring.size());
+        poolIndividuals.addAll(generation);
+        poolIndividuals.addAll(offspring);
+        PopulationImSeg selectionPool = new PopulationImSeg(problem, poolIndividuals);
+        ParetoImSeg sorted = NSGA2.FastNonDominatedSort(
+                selectionPool, (a, b) -> UtilChromoImSeg.dominates(problem, a, b));
+
+
+        List<ChromoImSeg> nextGeneration = new ArrayList<>(size);
+        int frontCount = 1;
+        var front = sorted.getFront(frontCount);
+        while (nextGeneration.size() + front.size() < size) {
+            nextGeneration.addAll(front);
+        }
+
+        front.sort(Comparator.comparingDouble(sorted::getCrowdingDistance));
+        //TODO: check if sorted the right way around!
+        while (nextGeneration.size() < size) {
+            nextGeneration.add(front.remove(0));
+        }
+
+        return new PopulationImSeg(problem, nextGeneration);
+
+
+    }
+}
